c2397725cc402a9fbbb6472da9883ce7e0217da3
Alexander Kadur <122496220+alex-kadur@users.noreply.github.com>
2024-03-05 11:45:52 +0100

    Update A_02_02_03.sql

--- Tag 2/Aufgabe 3/A_02_02_03.sql
+++ Tag 2/Aufgabe 3/A_02_02_03.sql
@@ -1,1 +1,39 @@
+CREATE DATABASE A_02_02_03;
+USE A_02_02_03;
+
+CREATE TABLE Stadt
+(
+    Stadt_ID INT(11) AUTO_INCREMENT,
+    Stadt_Name VARCHAR(255) NOT NULL,
+    PRIMARY KEY(Stadt_ID)
+);
+
+CREATE TABLE Stadtteil
+(
+    Stadtteil_ID INT(11) AUTO_INCREMENT,
+    Stadt_ID INT(11) NOT NULL,
+    Stadtteil_Name VARCHAR(255) NOT NULL,
+    PRIMARY KEY(Stadt_ID),
+    FOREIGN KEY(Stadt_ID) REFERENCES Stadt(Stadt_ID)
+);
+
+CREATE TABLE Postbote
+(
+    Postbote_ID INT(11) AUTO_INCREMENT,
+    Postbote_Vorname VARCHAR(255) NOT NULL,
+    Postbote_Nachname VARCHAR(255) NOT NULL,
+    PRIMARY KEY(Stadt_ID)
+);
+
+CREATE TABLE Strasse
+(
+    Strasse_ID INT(11) AUTO_INCREMENT,
+    Stadtteil_ID INT(11) NOT NULL,
+    Postbote_ID INT(11) NOT NULL,
+    Strasse_Name VARCHAR(255) NOT NULL,
+    PRIMARY KEY(Strasse_ID),
+    FOREIGN KEY(Stadtteil_ID) REFERENCES Stadtteil(Stadtteil_ID)
+    FOREIGN KEY(Postbote_ID) REFERENCES Postbote(Postbote_ID)
+);
+
 
