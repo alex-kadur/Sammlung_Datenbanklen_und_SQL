a92484be1af84acece1ddb3c4ea56f7f6fc49a21
Alexander Kadur <122496220+alex-kadur@users.noreply.github.com>
2024-03-05 09:14:43 +0100

    Update A_02_02_01.sql

--- Tag 2/A_02_02_01.sql
+++ Tag 2/A_02_02_01.sql
@@ -3,6 +3,29 @@ USE A_02_02_01;
 
 CREATE TABLE Firma
 (
-  Firma_ID INT(11) AUTO_INCREMENT;
-  
+  Firma_ID INT(11) AUTO_INCREMENT,
+  Firma_Name VARCHAR(255) NOT NULL,
+  PRIMARY KEY(Firma_ID)
 );
+
+CREATE TABLE Abteilung
+(
+  Abteilung_ID INT(11) AUTO_INCREMENT,
+  Firma_ID INT(11) NOT NULL,
+  Abteilung_Bezeichnung VARCHAR(255) NOT NULL,
+  PRIMARY KEY(Abteilung_ID),
+  FOREIGN KEY(Firma_ID) REFERENCES Firma(Firma_ID),
+);
+
+CREATE TABLE Person
+(
+  Person_ID INT(11) AUTO_INCREMENT,
+  Abteilung_ID INT(11) NOT NULL,
+  Person_Vorname VARCHAR(255) NOT NULL,
+  Person_Nachname VARCHAR(255) NOT NULL,
+  PRIMARY KEY(Person_ID),
+  FOREIGN KEY(Abteilung_ID) REFERENCES Abteilung(Abteilung_ID),
+);
+
+ALTER TABLE Firma ADD Person_ID INT(11) NOT NULL;
+ALTER TABLE Firma ADD FOREIGN KEY(Person_ID) REFERENCES Person(Person_ID);
