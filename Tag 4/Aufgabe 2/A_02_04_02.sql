fb2830c985bdf2b0322e4aecde7c3f856aa3aa08
Alexander Kadur <122496220+alex-kadur@users.noreply.github.com>
2024-03-10 15:44:03 +0100

    Update A_02_04_02.sql

--- Tag 4/Aufgabe 2/A_02_04_02.sql
+++ Tag 4/Aufgabe 2/A_02_04_02.sql
@@ -1,38 +1,46 @@
-#a)	
-ps>100 AND baujahr<1990;
+# a)
+DELETE FROM Autos
+WHERE ps > 100 AND baujahr < 1990;
 
-#b)	
-besitzer_id<12 AND preis>= 8000; #oder:  besitzer_id<12 AND NOT(preis<8000);
+# b)
+DELETE FROM Autos
+WHERE besitzer_id < 12 AND preis >= 8000;
 
-#c)	
-preis>10000 OR hersteller_id=5;
+# c)
+DELETE FROM Autos
+WHERE preis > 10000 OR hersteller_id = 5;
 
-#d)	baujahr<1970 XOR preis=12000;
+# d)
+DELETE FROM Autos
+WHERE baujahr < 1970 XOR preis=12000;
 
-#e)	preis<baujahr AND NOT(hersteller_id=7);
+# e)
+DELETE FROM Autos
+WHERE preis < baujahr AND NOT (hersteller_id = 7);
 
-#f)	NOT besitzer_id>10 AND NOT hersteller_id>10;
-#oder:  besitzer_id<=10 AND hersteller_id<=10;
-#oder:  NOT(besitzer_id>10 OR hersteller_id>10);
+# f)
+DELETE FROM Autos
+WHERE besitzer_id<=10 AND hersteller_id<=10;
 
-#g)	preis<ps AND id>baujahr;
+# g)
+DELETE FROM Autos
+WHERE preis < ps AND id > baujahr;
 
-#h)	ps>17 OR preis>17 OR id>17;
+# h)
+DELETE FROM Autos
+WHERE ps > 17 OR preis > 17 OR id > 17;
 
-#i)	ps>17 AND preis>17 AND id>17;
+#i)
+DELETE FROM Autos
+WHERE ps > 17 AND preis > 17 AND id > 17;
 
-#j)	ps>17 XOR preis>17 XOR id>17; #(Dies ist keine Lösung für Aufgabe (j))
-#(XOR-Ketten sind genau dann wahr, wenn die Anzahl der wahren Aussagen UNGERADE ist. 
-#Beispiel: A XOR B XOR C XOR D ist wahr, wenn 1 oder wenn 3 Aussagen wahr sind)
-#ACHTUNG: Klammern sind hier überflüssig (XOR ist „assoziativ“)
+#j)
+DELETE FROM Autos
+WHERE (ps > 17 XOR preis > 17 XOR id > 17) AND NOT (ps > 17 AND preis > 17 AND id > 17) 
+# erste Klammer wahr, wenn 1 Aussage ODER 3 Aussagen wahr
+# zweite Klammer schließt den Fall "3 wahre Aussagen" aus
 
-#Lösung (ohne XOR):
-(ps>17 AND NOT preis>17 AND NOT id>17) 
-OR 
-(NOT ps>17 AND preis>17 AND NOT id>17)
-OR
-(NOT ps>17 AND NOT preis>17 AND id>17);
-
-#Lösung (mit XOR):
-(ps > 17 XOR preis > 17 XOR id > 17)         # die erste Klammer ist wahr, wenn 1 Aussage ODER 3 Aussagen wahr
-AND NOT (ps > 17 AND preis > 17 AND id > 17) # die zweite Klammer schließt den Fall "3 wahre Aussagen" aus
+# Kommentar aus Musterloesung:
+# XOR-Ketten sind genau dann wahr, wenn die Anzahl der wahren Aussagen UNGERADE ist. 
+# Beispiel: A XOR B XOR C XOR D ist wahr, wenn 1 oder wenn 3 Aussagen wahr sind
+# ACHTUNG: Klammern sind hier überflüssig; XOR ist „assoziativ“
