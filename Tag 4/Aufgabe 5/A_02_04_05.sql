70d910060e90fe586ee203276dbe8b1e17c27b48
Alexander Kadur <122496220+alex-kadur@users.noreply.github.com>
2024-03-10 16:15:51 +0100

    Update A_02_04_05.sql

--- Tag 4/Aufgabe 5/A_02_04_05.sql
+++ Tag 4/Aufgabe 5/A_02_04_05.sql
@@ -32,3 +32,12 @@ CREATE TABLE Person_Lied
 
 /*----------*/
 
+ALTER TABLE Lied 
+ADD Person_ID INT(11) NOT NULL;
+
+ALTER TABLE Lied
+ADD FOREIGN KEY(Person_ID) REFERENCES Person(Person_ID);
+
+\*----------*\
+
+
