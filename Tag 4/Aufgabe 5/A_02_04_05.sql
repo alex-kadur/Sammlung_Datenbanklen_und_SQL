d61b7eb895ef6d979f4b48ec1bf28784e9b3b4bd
Alexander Kadur <122496220+alex-kadur@users.noreply.github.com>
2024-03-10 16:18:10 +0100

    Update A_02_04_05.sql

--- Tag 4/Aufgabe 5/A_02_04_05.sql
+++ Tag 4/Aufgabe 5/A_02_04_05.sql
@@ -38,6 +38,22 @@ ADD Person_ID INT(11) NOT NULL;
 ALTER TABLE Lied
 ADD FOREIGN KEY(Person_ID) REFERENCES Person(Person_ID);
 
-\*----------*\
+/*----------*/
+
+INSERT INTO Person(Vorname, Nachname, Geburtsjahr, Strasse, Hausnummer, PLZ, Ort)
+VALUES
+("Anton", "Amtmann", 1981, "Aue", 1, 52062, "Aachen"),
+("Bertha", "Baumann", 1982, "Baumweg", 2, 10115, "Berlin"),
+("Carla", "Carlson", 1983, "Cluse", 3, 09111, "Chemnitz");
+
+INSERT INTO Lied(Titel,Erscheinungsjahr,Person_ID)
+VALUES
+("Nothing Compares 2U", 1990, 1),
+("La Le Lu", 1950, 3);
+
+INSERT INTO Person_Lied
+VALUES(1, 1), (2, 1), (3, 1), (2, 2), (3, 2);
+
+/*----------*/
 
 
