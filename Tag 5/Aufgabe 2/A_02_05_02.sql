9e279a5134163e3b396e6f947da417c375c73d3f
Alexander Kadur <122496220+alex-kadur@users.noreply.github.com>
2024-03-11 12:06:27 +0100

    Update A_02_05_02.sql

--- Tag 5/Aufgabe 2/A_02_05_02.sql
+++ Tag 5/Aufgabe 2/A_02_05_02.sql
@@ -1,39 +1,32 @@
 # a)
-SELECT *
-FROM Produkt;
+SELECT * FROM Produkt;
 
 # b)
-SELECT *
-FROM Abrechnung_Produkt
+SELECT * FROM Abrechnung_Produkt
 WHERE Abrechnung_ID = 1 OR Abrechnung_ID = 4;
 
 # c)
-SELECT *
-FROM Abrechnung
+SELECT * FROM Abrechnung
 WHERE Kunde_ID = 1 OR Kunde_ID = 2 OR Kunde_ID = 3
 ORDER BY Datum DESC;
 
 # d)
-SELECT Produkt_Name, Euro_Preis
-FROM Produkt
+SELECT Produkt_Name, Euro_Preis FROM Produkt
 WHERE Hersteller_ID = 1 OR Hersteller_ID = 2
 ORDER BY Produkt_Name;
 
 # e)
-SELECT Kunde_ID, Datum
-FROM Abrechnung
+SELECT Kunde_ID, Datum FROM Abrechnung
 ORDER BY Kunde_ID, Datum DESC;
 
 # f)
-SELECT Vorname, Nachname
-FROM Kunde
+SELECT Vorname, Nachname FROM Kunde
 WHERE NOT Vorname = "Pietro" AND NOT Vorname = "Peter"
 ORDER BY Kunde_ID DESC
 LIMIT 2, 3;
 
 # g)
-SELECT DISTINCT Hersteller_ID
-FROM Produkt
+SELECT DISTINCT Hersteller_ID FROM Produkt
 WHERE Euro_Preis > 30
 ORDER BY Hersteller_ID; 
 
