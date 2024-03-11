dc65e634d04c5b981bbec84780ac43acd26104f8
Alexander Kadur <122496220+alex-kadur@users.noreply.github.com>
2024-03-11 10:31:41 +0100

    Create A_03_01_01.sql

---
+++ Tag 5/Aufgabe 1/A_03_01_01.sql
@@ -0,0 +1,34 @@
+# a)
+SELECT *
+FROM Spedition;
+
+# b)
+SELECT *
+FROM Hersteller
+ORDER BY Hersteller_Name;
+
+# c)
+SELECT *
+FROM Produkt
+WHERE Euro_Preis > 20 AND Euro_Preis < 50
+ORDER BY Produkt_Name, Euro_Preis;
+
+# d)
+SELECT Kunde_ID, Datum
+FROM Abrechnung
+WHERE Datum > "2021-05-31" AND Datum < "2022-06-01";
+
+# e)
+SELECT *
+FROM Kunde
+ORDER BY Kunde_ID
+DESC
+LIMIT 2, 4;
+
+# f)
+SELECT DISTINCT Kunde_ID
+FROM Abrechnung
+ORDER BY Kunde_ID;
+
+
+
