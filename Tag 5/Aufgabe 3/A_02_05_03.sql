f1dc06334e625617a77502cf854a776c2663827e
Alexander Kadur <122496220+alex-kadur@users.noreply.github.com>
2024-03-11 12:05:58 +0100

    Update A_02_05_03.sql

--- Tag 5/Aufgabe 3/A_02_05_03.sql
+++ Tag 5/Aufgabe 3/A_02_05_03.sql
@@ -1,32 +1,27 @@
 # a)
-SELECT *
-FROM Produkt
+SELECT * FROM Produkt
 WHERE ID > 1 AND (Euro_Preis > 20 AND Euro_Preis < 50)
 ORDER BY Herstller_ID, Euro_Preis DESC
 LIMIT 2, 3;
 
 # b)
-SELECT Spedition_Name
-FROM Spedition
+SELECT Spedition_Name FROM Spedition
 WHERE NOT (Spedition_ID = 1 OR Spedition_ID = 10)
 ORDER BY Spedition_Name
 LIMIT 2, 10;
 
 # c)
-SELECT Kunde_ID, Nachname
-FROM Kunde
+SELECT Kunde_ID, Nachname FROM Kunde
 WHERE NOT (Kunde_ID > 100 OR Nachname = "Mustermann")
 ORDER BY Kunde_ID DESC
 LIMIT 4;
 
 # d)
-SELECT DISTINCT Produkt_ID
-FROM Abrechnung_Produkt
+SELECT DISTINCT Produkt_ID FROM Abrechnung_Produkt
 WHERE Produkt_ID > 2 AND Abrechnung_ID > 3
 ORDER BY Produkt_ID;
 
 # e)
-SELECT *
-From Produkt
+SELECT * From Produkt
 ORDER BY Euro_Preis
 LIMIT 100, 3;
