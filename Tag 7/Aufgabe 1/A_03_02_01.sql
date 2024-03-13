3b9dbf93e78e7d6134aa2c1490026b57ab3bfcb9
Alexander Kadur <122496220+alex-kadur@users.noreply.github.com>
2024-03-13 11:20:37 +0100

    A_03_02_01.sql

--- Tag 7/Aufgabe 1/A_03_02_01.sql
+++ Tag 7/Aufgabe 1/A_03_02_01.sql
@@ -63,6 +63,7 @@ INSERT INTO Mitarbeiter VALUES
 (2,3,"Herr Klein"),
 (3,NULL,"Bürokraft");
 
+/* -----FULL JOIN----- */
 
 ########################################################################
 #  Folgende Ausgabe würde sich beim doppelten FULL JOIN also ergeben:  #       
@@ -91,4 +92,300 @@ FROM
 ##########################################################################
 #        NULL             #       NULL           #    Gäste-Parkplatz    #
 ##########################################################################    
- 
+
+/*-----*/
+	
+###################################################################
+#  Folgende Ausgabe würde sich beim doppelten INNER JOIN ergeben: #       
+###################################################################
+/*
+SELECT 
+    mitarbeiter.mitarbeiter_bezeichnung,
+    fahrzeug.fahrzeug_bezeichnung,
+    parkplatz.parkplatz_bezeichnung
+FROM 
+	mitarbeiter INNER JOIN fahrzeug ON mitarbeiter.fahrzeug_id=fahrzeug.fahrzeug_id
+	INNER JOIN parkplatz ON fahrzeug.parkplatz_id=parkplatz.parkplatz_id;
+*/
+##################################################
+# mitarbeiter_bezeichnung # fahrzeug_bezeichnung #	
+##################################################
+#       Chef              #      Porsche         #
+##################################################
+#      Herr Klein         #     Kleinwagen       #
+##################################################
+	
+##########################################################################
+# mitarbeiter_bezeichnung # fahrzeug_bezeichnung # parkplatz_bezeichnung #
+##########################################################################
+#       Chef              #      Porsche         #     Chef-Parkplatz    #
+##########################################################################
+
+/* -----1 INNER JOIN----- */
+	
+####################################################################
+#  Folgende Ausgabe würde sich bei INNER JOIN > LEFT JOIN ergeben: #       
+####################################################################
+/*
+SELECT 
+    mitarbeiter.mitarbeiter_bezeichnung,
+    fahrzeug.fahrzeug_bezeichnung,
+    parkplatz.parkplatz_bezeichnung
+FROM 
+	mitarbeiter INNER JOIN fahrzeug ON mitarbeiter.fahrzeug_id=fahrzeug.fahrzeug_id
+	LEFT JOIN parkplatz ON fahrzeug.parkplatz_id=parkplatz.parkplatz_id;
+*/
+##################################################
+# mitarbeiter_bezeichnung # fahrzeug_bezeichnung #	
+##################################################
+#       Chef              #      Porsche         #
+##################################################
+#      Herr Klein         #     Kleinwagen       #
+##################################################
+
+##########################################################################
+# mitarbeiter_bezeichnung # fahrzeug_bezeichnung # parkplatz_bezeichnung #
+##########################################################################
+#       Chef              #      Porsche         #     Chef-Parkplatz    #
+##########################################################################
+#      Herr Klein         #     Kleinwagen       #        NULL           #
+##########################################################################
+
+/* ----- */
+	
+#####################################################################
+#  Folgende Ausgabe würde sich bei INNER JOIN > RIGHT JOIN ergeben: #       
+#####################################################################
+/*
+SELECT 
+    mitarbeiter.mitarbeiter_bezeichnung,
+    fahrzeug.fahrzeug_bezeichnung,
+    parkplatz.parkplatz_bezeichnung
+FROM 
+	mitarbeiter INNER JOIN fahrzeug ON mitarbeiter.fahrzeug_id=fahrzeug.fahrzeug_id
+	RIGHT JOIN parkplatz ON fahrzeug.parkplatz_id=parkplatz.parkplatz_id;
+*/
+##################################################
+# mitarbeiter_bezeichnung # fahrzeug_bezeichnung #	
+##################################################
+#       Chef              #      Porsche         #
+##################################################
+#      Herr Klein         #     Kleinwagen       #
+##################################################
+
+##########################################################################
+# mitarbeiter_bezeichnung # fahrzeug_bezeichnung # parkplatz_bezeichnung #
+##########################################################################
+#       Chef              #      Porsche         #     Chef-Parkplatz    #
+##########################################################################
+#        NULL             #       NULL           #    LKW-Parkplatz      #
+##########################################################################
+#        NULL             #       NULL           #    Gäste-Parkplatz    #
+##########################################################################
+
+/* -----1 LEFT JOIN----- */
+	
+#####################################################################
+#  Folgende Ausgabe würde sich bei LEFT JOIN > INNER JOIN ergeben: #       
+#####################################################################
+/*
+SELECT 
+    mitarbeiter.mitarbeiter_bezeichnung,
+    fahrzeug.fahrzeug_bezeichnung,
+    parkplatz.parkplatz_bezeichnung
+FROM 
+	mitarbeiter LEFT JOIN fahrzeug ON mitarbeiter.fahrzeug_id=fahrzeug.fahrzeug_id
+	INNER JOIN parkplatz ON fahrzeug.parkplatz_id=parkplatz.parkplatz_id;
+*/
+##################################################
+# mitarbeiter_bezeichnung # fahrzeug_bezeichnung #
+##################################################
+#       Chef              #      Porsche         #
+##################################################
+#      Herr Klein         #     Kleinwagen       #
+##################################################
+#       Bürokraft 	  #       NULL           #
+##################################################
+
+##########################################################################
+# mitarbeiter_bezeichnung # fahrzeug_bezeichnung # parkplatz_bezeichnung #
+##########################################################################
+#       Chef              #      Porsche         #     Chef-Parkplatz    #
+##########################################################################
+
+/* ----- */
+	
+#####################################################################
+#  Folgende Ausgabe würde sich bei LEFT JOIN > LEFT JOIN ergeben: #       
+#####################################################################
+/*
+SELECT 
+    mitarbeiter.mitarbeiter_bezeichnung,
+    fahrzeug.fahrzeug_bezeichnung,
+    parkplatz.parkplatz_bezeichnung
+FROM 
+	mitarbeiter LEFT JOIN fahrzeug ON mitarbeiter.fahrzeug_id=fahrzeug.fahrzeug_id
+	LEFT JOIN parkplatz ON fahrzeug.parkplatz_id=parkplatz.parkplatz_id;
+*/
+##################################################
+# mitarbeiter_bezeichnung # fahrzeug_bezeichnung #
+##################################################
+#       Chef              #      Porsche         #
+##################################################
+#      Herr Klein         #     Kleinwagen       #
+##################################################
+#       Bürokraft 	  #       NULL           #
+##################################################
+
+##########################################################################
+# mitarbeiter_bezeichnung # fahrzeug_bezeichnung # parkplatz_bezeichnung #
+##########################################################################
+#       Chef              #      Porsche         #     Chef-Parkplatz    #
+##########################################################################
+#      Herr Klein         #     Kleinwagen       #        NULL           #
+##########################################################################
+#       Bürokraft 	  #       NULL           #        NULL           #
+##########################################################################
+
+/* ----- */
+	
+#####################################################################
+#  Folgende Ausgabe würde sich bei LEFT JOIN > RIGHT JOIN ergeben: #       
+#####################################################################
+/*
+SELECT 
+    mitarbeiter.mitarbeiter_bezeichnung,
+    fahrzeug.fahrzeug_bezeichnung,
+    parkplatz.parkplatz_bezeichnung
+FROM 
+	mitarbeiter LEFT JOIN fahrzeug ON mitarbeiter.fahrzeug_id=fahrzeug.fahrzeug_id
+	RIGHT JOIN parkplatz ON fahrzeug.parkplatz_id=parkplatz.parkplatz_id;
+*/
+##################################################
+# mitarbeiter_bezeichnung # fahrzeug_bezeichnung #
+##################################################
+#       Chef              #      Porsche         #
+##################################################
+#      Herr Klein         #     Kleinwagen       #
+##################################################
+#       Bürokraft 	  #       NULL           #
+##################################################
+
+##########################################################################
+# mitarbeiter_bezeichnung # fahrzeug_bezeichnung # parkplatz_bezeichnung #
+##########################################################################
+#       Chef              #      Porsche         #     Chef-Parkplatz    #
+##########################################################################
+#        NULL             #       NULL           #    LKW-Parkplatz      #
+##########################################################################
+#        NULL             #       NULL           #    Gäste-Parkplatz    #
+##########################################################################
+
+/* -----1 RIGHT JOIN----- */
+	
+#####################################################################
+#  Folgende Ausgabe würde sich bei RIGHT JOIN > INNER JOIN ergeben: #       
+#####################################################################
+/*
+SELECT 
+    mitarbeiter.mitarbeiter_bezeichnung,
+    fahrzeug.fahrzeug_bezeichnung,
+    parkplatz.parkplatz_bezeichnung
+FROM 
+	mitarbeiter RIGHT JOIN fahrzeug ON mitarbeiter.fahrzeug_id=fahrzeug.fahrzeug_id
+	INNER JOIN parkplatz ON fahrzeug.parkplatz_id=parkplatz.parkplatz_id;
+*/
+##################################################
+# mitarbeiter_bezeichnung # fahrzeug_bezeichnung #
+##################################################
+#       Chef              #      Porsche         #
+##################################################
+#      Herr Klein         #     Kleinwagen       #
+##################################################
+#        NULL             #       LKW            #
+##################################################
+#        NULL             #    Schrott-Karre     #
+##################################################
+
+##########################################################################
+# mitarbeiter_bezeichnung # fahrzeug_bezeichnung # parkplatz_bezeichnung #
+##########################################################################
+#       Chef              #      Porsche         #     Chef-Parkplatz    #
+##########################################################################
+#        NULL             #       LKW            #    LKW-Parkplatz      #
+##########################################################################
+
+/* ----- */
+
+#####################################################################
+#  Folgende Ausgabe würde sich bei RIGHT JOIN > LEFT JOIN ergeben: #       
+#####################################################################
+/*
+SELECT 
+    mitarbeiter.mitarbeiter_bezeichnung,
+    fahrzeug.fahrzeug_bezeichnung,
+    parkplatz.parkplatz_bezeichnung
+FROM 
+	mitarbeiter RIGHT JOIN fahrzeug ON mitarbeiter.fahrzeug_id=fahrzeug.fahrzeug_id
+	LEFT JOIN parkplatz ON fahrzeug.parkplatz_id=parkplatz.parkplatz_id;
+*/
+##################################################
+# mitarbeiter_bezeichnung # fahrzeug_bezeichnung #
+##################################################
+#       Chef              #      Porsche         #
+##################################################
+#      Herr Klein         #     Kleinwagen       #
+##################################################
+#        NULL             #       LKW            #
+##################################################
+#        NULL             #    Schrott-Karre     #
+##################################################
+
+##########################################################################
+# mitarbeiter_bezeichnung # fahrzeug_bezeichnung # parkplatz_bezeichnung #
+##########################################################################
+#       Chef              #      Porsche         #     Chef-Parkplatz    #
+##########################################################################
+#      Herr Klein         #     Kleinwagen       #        NULL           #
+##########################################################################
+#        NULL             #       LKW            #    LKW-Parkplatz      #
+##########################################################################
+#        NULL             #    Schrott-Karre     #        NULL           #
+##########################################################################
+  
+/* ----- */
+
+#####################################################################
+#  Folgende Ausgabe würde sich bei RIGHT JOIN > RIGHT JOIN ergeben: #       
+#####################################################################
+/*
+SELECT 
+    mitarbeiter.mitarbeiter_bezeichnung,
+    fahrzeug.fahrzeug_bezeichnung,
+    parkplatz.parkplatz_bezeichnung
+FROM 
+	mitarbeiter RIGHT JOIN fahrzeug ON mitarbeiter.fahrzeug_id=fahrzeug.fahrzeug_id
+	RIGHT JOIN parkplatz ON fahrzeug.parkplatz_id=parkplatz.parkplatz_id;
+*/
+##################################################
+# mitarbeiter_bezeichnung # fahrzeug_bezeichnung #
+##################################################
+#       Chef              #      Porsche         #
+##################################################
+#      Herr Klein         #     Kleinwagen       #
+##################################################
+#        NULL             #       LKW            #
+##################################################
+#        NULL             #    Schrott-Karre     #
+##################################################
+
+##########################################################################
+# mitarbeiter_bezeichnung # fahrzeug_bezeichnung # parkplatz_bezeichnung #
+##########################################################################
+#       Chef              #      Porsche         #     Chef-Parkplatz    #
+##########################################################################
+#        NULL             #       LKW            #    LKW-Parkplatz      #
+##########################################################################
+#        NULL             #       NULL           #    Gäste-Parkplatz    #
+##########################################################################   
+
