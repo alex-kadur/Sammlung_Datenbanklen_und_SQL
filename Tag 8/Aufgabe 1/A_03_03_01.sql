cf6911245baa7c6b5096b08f6811d9f64c4ae7dd
Alexander Kadur <122496220+alex-kadur@users.noreply.github.com>
2024-03-14 14:52:21 +0100

    Update A_03_03_01.sql

--- Tag 8/Aufgabe 1/A_03_03_01.sql
+++ Tag 8/Aufgabe 1/A_03_03_01.sql
@@ -4,9 +4,8 @@ FROM spedition;
 
 # b)
 SELECT AVG(Euro_Preis) AS "Durchschnittspreis"
-FROM abrechnung_produkt
-LEFT JOIN produkt
-ON abrechnung_produkt.Produkt_ID = produkt.Produkt_ID;
+FROM abrechnung_produkt, produkt
+WHERE abrechnung_produkt.Produkt_ID = produkt.Produkt_ID;
 
 # c)
 SELECT Datum, SUM(Euro_Preis)
@@ -17,9 +16,24 @@ GROUP BY abrechnung.Abrechnung_ID
 ORDER BY SUM(Euro_Preis) DESC;
 
 # d)
-SELECT kunde.Kunde_ID, Nachname, SUM(Produkt_ID)
+SELECT kunde.Kunde_ID, Nachname, COUNT(Produkt_ID)
 FROM kunde, abrechnung, abrechnung_produkt
 WHERE kunde.Kunde_ID = abrechnung.Kunde_ID
 AND abrechnung.Abrechnung_ID = abrechnung_produkt.Abrechnung_ID
-GROUP BY Kunde_ID
-ORDER BY SUM(Produkt_ID) DESC, Nachname
+GROUP BY kunde.Kunde_ID
+ORDER BY COUNT(Produkt_ID) DESC, Nachname;
+
+# e)
+SELECT Hersteller_Name, COUNT(Produkt_ID)
+FROM hersteller, produkt
+WHERE hersteller.Hersteller_ID = produkt.Hersteller_ID
+GROUP BY Hersteller_ID  
+ORDER BY COUNT(Produkt_ID) DESC;
+
+# f)
+SELECT Produkt_Name, COUNT(produkt.Produkt_ID)
+FROM abrechnung_produkt, produkt
+WHERE abrechnung_produkt.Produkt_ID = produkt.Produkt_ID
+GROUP BY Produkt_ID
+ORDER BY COUNT(produkt.Produkt_ID) DESC LIMIT 3;
+
