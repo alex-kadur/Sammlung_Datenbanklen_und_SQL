52175fb5f58ee2e6b1fe9a88e8ea9f57dba772e2
Alexander Kadur <122496220+alex-kadur@users.noreply.github.com>
2024-03-15 12:37:22 +0100

    Update A_03_04_02.sql

--- Tag 9/Aufgabe 2/A_03_04_02.sql
+++ Tag 9/Aufgabe 2/A_03_04_02.sql
@@ -14,31 +14,42 @@ FROM hersteller
 LEFT JOIN produkt
 ON hersteller.Hersteller_ID = produkt.Hersteller_ID 
 GROUP BY hersteller.Hersteller_ID
-ORDER BY AVG(Euro_Preis) DESC;
+ORDER BY AVG(Euro_Preis DESC;
 
 # c)
 SELECT spedition.Spedition_ID, Spedition_Name, ifNULL(SUM(Euro_Preis), "keine Produkte transportiert")
-FROM spedition
-LEFT JOIN hersteller
-ON spedition.Spedition_ID = hersteller.Spedition_ID
+FROM abrechnung_produkt
 LEFT JOIN produkt
-ON hersteller.Hersteller_ID = produkt.Hersteller_ID
-LEFT JOIN abrechnung_produkt
 ON produkt.Produkt_ID = abrechnung_produkt.Produkt_ID
+LEFT JOIN hersteller
+ON hersteller.Hersteller_ID = produkt.Hersteller_ID  
+RIGHT JOIN spedition  
+ON spedition.Spedition_ID = hersteller.Spedition_ID
 GROUP BY spedition.Spedition_ID
-HAVING COUNT(abrechnung_produkt.Produkt_ID) > 0
 ORDER BY SUM(Euro_Preis) DESC;
 
 # d)
 SELECT produkt.Produkt_ID, Produkt_Name, COUNT(abrechnung_Produkt.Produkt_ID)
-FROM produkt
-LEFT JOIN abrechnung_produkt
-ON produkt.Produkt_ID = abrechnung_produkt.Produkt_ID
-LEFT JOIN abrechnung
-ON abrechnung.Abrechnung_ID = abrechnung_produkt.Abrechnung_ID
-LEFT JOIN kunde
+FROM kunde
+RIGHT JOIN abrechnung
 ON kunde.Kunde_ID = abrechnung.Kunde_ID
-WHERE Vorname = "Vera" AND Nachname = "Diese"
+RIGHT JOIN abrechnung_produkt
+ON abrechnung.Abrechnung_ID = abrechnung_produkt.Abrechnung_ID
+RIGHT JOIN produkt
+ON produkt.Produkt_ID = abrechnung_produkt.Produkt_ID
+AND Vorname = "Vera" AND Nachname = "Deise"  
 GROUP BY produkt.Produkt_ID
 ORDER BY COUNT(abrechnung_Produkt.Produkt_ID) DESC;
+
+# e)
+SELECT abrechnung.Abrechnung_ID, Datum, ifNULL(SUM(Euro_Preis), 0)
+FROM abrechnung
+LEFT JOIN abrechnung_produkt
+ON abrechnung.Abrechnung_ID = abrechnung_produkt.Abrechnung_ID
+LEFT JOIN produkt
+ON produkt.Produkt_ID = abrechnung_produkt.Produkt_ID
+GROUP BY abrechnung.Abrechnung_ID
+HAVING ifNULL(SUM(Euro_Preis), 0) <= 100
+ORDER BY ifNULL(SUM(Euro_Preis), 0);
+
 
