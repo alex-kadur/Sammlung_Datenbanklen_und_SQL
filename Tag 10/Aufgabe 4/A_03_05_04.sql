27a89766c4aae51de976a0049ee528167b1c44d3
Alexander Kadur <122496220+alex-kadur@users.noreply.github.com>
2024-03-20 17:37:55 +0100

    Update A_03_05_04.sql

--- Tag 10/Aufgabe 4/A_03_05_04.sql
+++ Tag 10/Aufgabe 4/A_03_05_04.sql
@@ -1,1 +1,11 @@
+# a) Gesucht werden alle Häuser, die in dem Wohnort von Hausbesitzer(ID=2) stehen.
 
+# b) Gesucht werden alle Standorte von jenen Häusern, deren Wohnfläche größer als die Wohnfläche des Hauses mit der ID=3 ist.
+
+# c) Gesucht werden alle Geschäfte, die sich in Häusern des Hausbesitzers mit ID=3 befinden.
+
+# d) Gesucht werden alle Häuser, deren Eigentümer (mindestens auch) ein Haus an ihrem eigenen Wohnort besitzen.
+
+# e) Gesucht werden alle Mieter des Hauses (oder der Häuser) mit maximaler Wohnfläche.
+
+# f) Gesucht werden alle Mieter von Häusern mit (mindestens) einem Ladenlokal.
